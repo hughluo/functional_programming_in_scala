df8be653a7ed2b178414c2669fe79ca72cfa0c05
wexort <bugwriter@outlook.com>
2019-12-13 09:28:22 +0100

    implement pascal triangle

---
+++ functional_programming_principles_in_scala/recfun/src/main/scala/recfun/RecFun.scala
@@ -0,0 +1,30 @@
+package recfun
+
+object RecFun extends RecFunInterface {
+
+  def main(args: Array[String]): Unit = {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(s"${pascal(col, row)} ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+  def pascal(c: Int, r: Int): Int = {
+    if (r == 0 || c == 0 || c == r) 1 else pascal(c - 1, r - 1) + pascal(c, r - 1)
+  }
+
+  /**
+   * Exercise 2
+   */
+  def balance(chars: List[Char]): Boolean = true
+
+  /**
+   * Exercise 3
+   */
+  def countChange(money: Int, coins: List[Int]): Int = 0
+}
