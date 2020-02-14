b3ba52c44a93c9964c99ab5f9f1774a145946e93
Wexort <bugwriter@outlook.com>
2020-02-14 10:47:01 +0100

    implement computeDelta

--- functional_program_design_in_scala/calculator/src/main/scala/calculator/Polynomial.scala
+++ functional_program_design_in_scala/calculator/src/main/scala/calculator/Polynomial.scala
@@ -3,7 +3,9 @@ package calculator
 object Polynomial extends PolynomialInterface {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
-    ???
+    Signal {
+      b() * b() - 4 * a() * c()
+    }
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
