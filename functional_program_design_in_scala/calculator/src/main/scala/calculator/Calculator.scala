70398527a875a6ecf555ad0769c90eac271866ea
Wexort <bugwriter@outlook.com>
2020-02-14 18:52:40 +0100

    implement calculator

--- functional_program_design_in_scala/calculator/src/main/scala/calculator/Calculator.scala
+++ functional_program_design_in_scala/calculator/src/main/scala/calculator/Calculator.scala
@@ -1,5 +1,7 @@
 package calculator
 
+import scala.util.{Try, Success, Failure}
+
 sealed abstract class Expr
 final case class Literal(v: Double) extends Expr
 final case class Ref(name: String) extends Expr
@@ -11,12 +13,24 @@ final case class Divide(a: Expr, b: Expr) extends Expr
 object Calculator extends CalculatorInterface {
   def computeValues(
       namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    ???
+    namedExpressions map {
+      case(key, value) => (key, Signal(eval(value(), namedExpressions)))
+    }
   }
 
-  def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
-    ???
+  def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = expr match {
+    case Literal(v) => v
+    case Ref(name) => if (isRefExist(name, references)) eval(references(name)(), references - name) else Double.NaN
+    case Plus(a, b) => eval(a, references) + eval(b, references)
+    case Minus(a, b) => eval(a, references) - eval(b, references)
+    case Times(a, b) => eval(a, references) * eval(b, references)
+    case Divide(a, b) => eval(a, references) / eval(b, references)
   }
+
+  def isRefExist(name: String, references: Map[String, Signal[Expr]]): Boolean = {
+    if (!references.contains(name)) false else true
+  }
+
 
   /** Get the Expr for a referenced variables.
    *  If the variable is not known, returns a literal NaN.
