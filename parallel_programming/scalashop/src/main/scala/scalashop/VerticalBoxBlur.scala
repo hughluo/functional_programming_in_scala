b949946cf11230a32832dbf0ef96eceede6c6bc7
Wexort <bugwriter@outlook.com>
2020-02-25 09:08:08 +0100

    implement fromToPair

--- parallel_programming/scalashop/src/main/scala/scalashop/VerticalBoxBlur.scala
+++ parallel_programming/scalashop/src/main/scala/scalashop/VerticalBoxBlur.scala
@@ -63,7 +63,16 @@ object VerticalBoxBlur extends VerticalBoxBlurInterface {
    */
   def parBlur(src: Img, dst: Img, numTasks: Int, radius: Int): Unit = {
     // TODO implement using the `task` construct and the `blur` method
-    ???
+
   }
+
+  def fromToPair(width: Int, numTasks: Int) = {
+    val step = (width + 1) / numTasks
+    val splittingPts = 0 to (width - 1) by step
+    val fromLst = splittingPts.toList
+    val toList = fromLst map (x => Math.min(x + step, width))
+    fromLst zip toList
+  }
+
 
 }
--- parallel_programming/scalashop/src/test/scala/scalashop/BlurSuite.scala
+++ parallel_programming/scalashop/src/test/scala/scalashop/BlurSuite.scala
@@ -7,5 +7,16 @@ import org.junit.Assert.assertEquals
 
 class BlurSuite {
 
+  import VerticalBoxBlur.fromToPair
+
+  @Test def `fromToPair test`: Unit = {
+    val res0 = List((0, 2), (2, 4), (4, 6))
+    assertEquals(res0, fromToPair(6, 3))
+
+    val res1 = List((0, 2), (2, 4), (4, 5))
+    assertEquals(res1, fromToPair(5, 3))
+
+  }
+
   @Rule def individualTestTimeout = new org.junit.rules.Timeout(10 * 1000)
 }
